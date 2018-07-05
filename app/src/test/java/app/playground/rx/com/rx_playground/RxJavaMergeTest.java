b387b2aa2192bfe3044f72a0352841dc476d15c9
Anatolii <resident234@gmail.com>
2018-07-05 09:57:24 +0200

    Do for loop the JAVA 8 way

--- app/src/test/java/app/playground/rx/com/rx_playground/RxJavaMergeTest.java
+++ app/src/test/java/app/playground/rx/com/rx_playground/RxJavaMergeTest.java
@@ -4,19 +4,27 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.IntStream;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 
 public class RxJavaMergeTest {
     @Test
     public void basicObservableMerge() {
         List<Observable<Integer>> observables = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
-            final int r = i;
-            Observable<Integer> observable = Observable.defer(() -> Observable.just(r));
-            observables.add(observable);
-        }
+        IntStream.range(0, 5)
+                .forEach(i -> {
+                    Observable<Integer> observable = Observable.defer(new Callable<ObservableSource<? extends Integer>>() {
+                        @Override
+                        public ObservableSource<? extends Integer> call() throws Exception {
+                            return Observable.just(i);
+                        }
+                    });
+                    observables.add(observable);
+                });
 
         Observable.merge(observables)
                 .subscribe(
